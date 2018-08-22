1ecf482e8fc48fa0b356055afcdd125d61559111
nullice <ui@Nullice.com>
2018-08-23 00:26:36 +0800

    fix HSL_to_RGB

--- src/Core/Converters/HSL_to_RGB.ts
+++ src/Core/Converters/HSL_to_RGB.ts
@@ -9,63 +9,39 @@ import util from "@/Core/util"
  * @param out
  * @constructor
  */
-function HSL_to_RGB(hsl: objectHSL, out?: any): objectRGB {
+function HSL_to_RGB(hsl: objectHSL, out?: any): objectRGB
+{
     if (out === undefined) out = {};
 
-    let h = hsl.h;
+
+    let h = hsl.h == 360 ? 0 : hsl.h / 60;
     let s = util.normaliz(hsl.s, 0, 100, 1);
     let l = util.normaliz(hsl.l, 0, 100, 1);
 
-    if (h == 360) h = 0;
+    let t2
+    if( l <= .5 ) {
+        t2 = l * (s + 1);
+    } else {
+        t2 = l + s - (l * s);
+    }
+    let t1 = l * 2 - t2;
 
-    let C = (1 - Math.abs((2 * l) - 1)) * s;
-    let hh = h / 60;
-    let temp = C * (1 - Math.abs((hh % 2) - 1));
-    hh = Math.floor(hh);
-
-    let r = 0, g = 0, b = 0;
-    if (hh === 0) {
-        r = C;
-        g = temp;
-        b = 0
-    }
-    else if (hh === 1) {
-        r = temp;
-        g = C;
-        b = 0
-    }
-    else if (hh === 2) {
-        r = 0;
-        g = C;
-        b = temp
-    }
-    else if (hh === 3) {
-        r = 0;
-        g = temp;
-        b = C
-    }
-    else if (hh === 4) {
-        r = temp;
-        g = 0;
-        b = C
-    }
-    else (hh === 5);
-    {
-        r = C;
-        g = 0;
-        b = temp
-    }
-
-    let CC = l - (C / 2);
-    r += CC;
-    g += CC;
-    b += CC;
-
-    out.r = util.normaliz(r, 0, 1, 255);
-    out.g = util.normaliz(g, 0, 1, 255);
-    out.b = util.normaliz(b, 0, 1, 255);
+    out.r = util.normalInt256(hueToRgb(t1, t2, h + 2) * 255)
+    out.g = util.normalInt256(hueToRgb(t1, t2, h) * 255)
+    out.b = util.normalInt256(hueToRgb(t1, t2, h - 2) * 255)
 
     return out;
+
+    function hueToRgb(t1: number, t2: number, hue: number)
+    {
+
+        if (hue < 0) hue += 6;
+        if (hue >= 6) hue -= 6;
+        if (hue < 1) return (t2 - t1) * hue + t1;
+        if (hue < 3) return t2;
+        if (hue < 4) return (t2 - t1) * (4 - hue) + t1;
+        return t1;
+    }
 }
 
 
